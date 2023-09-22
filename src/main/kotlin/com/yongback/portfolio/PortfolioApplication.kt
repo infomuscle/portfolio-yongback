0ac6dbf19364d71b020e385de52f7c2b0b58cb90
infomuscle <infomuscle10@gmail.com>
2023-09-22 15:03:28 +0900

    Git과 Github

---
+++ src/main/kotlin/com/yongback/portfolio/PortfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.yongback.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioApplication>(*args)
+}
---
+++ src/test/kotlin/com/yongback/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yongback.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
