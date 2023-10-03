4e4fcb295c47dde9ada9713351f7bf080bcf5ec2
infomuscle <infomuscle10@gmail.com>
2023-10-03 21:42:56 +0900

    인터셉터 개발

--- src/main/kotlin/com/yongback/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/yongback/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,8 +1,20 @@
 package com.yongback.portfolio.presentation.interceptor
 
+import com.yongback.portfolio.domain.entity.HttpInterface
+import com.yongback.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 
 }
