3cf9b3f618e997851c7427d1ad4e306f25cdeb42
infomuscle <infomuscle10@gmail.com>
2023-11-07 20:22:39 +0900

    리포지토리 성능 개선

--- src/main/kotlin/com/yongback/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/yongback/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,9 +2,15 @@ package com.yongback.portfolio.domain.repository
 
 import com.yongback.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
 
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
+
+    @Query("select e from Experience e left join fetch e.details where e.id = :id")
+    override fun findById(id: Long): Optional<Experience>
 
 }
