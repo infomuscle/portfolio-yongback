ba2b16c376248f58ef83d88a6ebba50214aadfdb
infomuscle <infomuscle10@gmail.com>
2023-11-11 18:59:05 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/yongback/portfolio/domain/entity/Introduction.kt
+++ src/main/kotlin/com/yongback/portfolio/domain/entity/Introduction.kt
@@ -7,11 +7,15 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
-class Introduction : BaseEntity() {
+class Introduction(content: String, isActive: Boolean) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "introduction_id")
     var id: Long? = null
+
+    var content: String = content
+
+    var isActive: Boolean = isActive
 
 }
--- src/main/kotlin/com/yongback/portfolio/domain/entity/Link.kt
+++ src/main/kotlin/com/yongback/portfolio/domain/entity/Link.kt
@@ -7,11 +7,17 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
-class Link : BaseEntity() {
+class Link(name: String, content: String, isActive: Boolean) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "link_id")
     var id: Long? = null
+
+    var name: String = name
+
+    var content: String = content
+
+    var isActive: Boolean = isActive
 
 }
