ed244c92520712decbaceb853ce0930277444fa3
infomuscle <infomuscle10@gmail.com>
2024-12-22 04:43:30 +0900

    리포지토리 성능 개선

--- src/main/kotlin/com/yongback/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/yongback/portfolio/domain/repository/ProjectRepository.kt
@@ -2,9 +2,14 @@ package com.yongback.portfolio.domain.repository
 
 import com.yongback.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface ProjectRepository : JpaRepository<Project, Long> {
 
+    @Query("select p from Project p left join fetch p.skills s left join fetch s.skill where p.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Project>
 
+    @Query("select p from Project p left join fetch p.details where p.id = :id")
+    override fun findById(id: Long): Optional<Project>
 }
