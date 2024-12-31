14fa7c843a7938aaabc0a2b97c2d21f69ef4b228
infomuscle <infomuscle10@gmail.com>
2024-12-31 18:23:14 +0900

    컨트롤러 개발

---
+++ src/main/kotlin/com/yongback/portfolio/presentation/controller/PresentationApiController.kt
@@ -0,0 +1,43 @@
+package com.yongback.portfolio.presentation.controller
+
+import com.yongback.portfolio.presentation.dto.IntroductionDTO
+import com.yongback.portfolio.presentation.dto.LinkDTO
+import com.yongback.portfolio.presentation.dto.ProjectDTO
+import com.yongback.portfolio.presentation.dto.ResumeDTO
+import com.yongback.portfolio.presentation.service.PresentationService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "OK"
+    }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
+    }
+
+}
---
+++ src/main/kotlin/com/yongback/portfolio/presentation/controller/PresentationViewController.kt
@@ -0,0 +1,50 @@
+package com.yongback.portfolio.presentation.controller
+
+import com.yongback.portfolio.domain.constant.SkillType
+import com.yongback.portfolio.presentation.service.PresentationService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class PresentationViewController(
+    private val presentationService: PresentationService
+) {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "test"
+    }
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+
+        val introductions = presentationService.getIntroductions()
+        model.addAttribute("introductions", introductions)
+
+        val links = presentationService.getLinks()
+        model.addAttribute("links", links)
+
+        return "presentation/index"
+    }
+
+    @GetMapping("/resume")
+    fun resume(model: Model): String {
+
+        val resume = presentationService.getResume()
+        model.addAttribute("resume", resume)
+        model.addAttribute("skillTypes", SkillType.values())
+
+        return "presentation/resume"
+    }
+
+    @GetMapping("/projects")
+    fun projects(model: Model): String {
+
+        val projects = presentationService.getProjects()
+        model.addAttribute("projects", projects)
+
+        return "presentation/projects"
+    }
+
+}
