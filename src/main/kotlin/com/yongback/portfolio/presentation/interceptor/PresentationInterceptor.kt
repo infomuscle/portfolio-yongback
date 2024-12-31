d83093c3329b521ac5b4ad977565baee2d29c348
infomuscle <infomuscle10@gmail.com>
2025-01-01 02:06:02 +0900

    인터셉터 개발

---
+++ src/main/kotlin/com/yongback/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -0,0 +1,25 @@
+package com.yongback.portfolio.presentation.interceptor
+
+import com.yongback.portfolio.domain.entity.HttpInterface
+import com.yongback.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+
+@Component
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+
+}
---
+++ src/main/kotlin/com/yongback/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -0,0 +1,26 @@
+package com.yongback.portfolio.presentation.interceptor
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class PresentationInterceptorConfiguration(
+    private val presentationInterceptor: PresentationInterceptor
+) : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationInterceptor)
+            .addPathPatterns("/**")
+            .excludePathPatterns(
+                "/assets/**",
+                "/css/**",
+                "/js/**",
+                "/admin/**",
+                "/h2**",
+                "/favicon.ico",
+                "/error"
+            )
+    }
+
+}
