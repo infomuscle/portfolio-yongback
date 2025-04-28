79cf8c01e317af00a394d8878f5714bd6043f157
infomuscle <infomuscle10@gmail.com>
2025-04-29 00:25:39 +0900

    공통 개발 - 인터셉터

---
+++ src/main/kotlin/com/yongback/portfolio/admin/interceptor/AdminInterceptor.kt
@@ -0,0 +1,46 @@
+package com.yongback.portfolio.admin.interceptor
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.ModelAndView
+
+@Component
+class AdminInterceptor : HandlerInterceptor {
+
+    override fun postHandle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        modelAndView: ModelAndView?
+    ) {
+        val menus = listOf<MenuDTO>(
+            MenuDTO(
+                name = "Index",
+                pages = listOf<PageDTO>(
+                    PageDTO(name = "Introduction", url = "/admin/introduction"),
+                    PageDTO(name = "Link", url = "/admin/link")
+                )
+            ),
+            MenuDTO(
+                name = "Resume",
+                pages = listOf<PageDTO>(
+                    PageDTO(name = "Experience", url = "/admin/experience"),
+                    PageDTO(name = "Achievement", url = "/admin/achievement"),
+                    PageDTO(name = "Skill", url = "/admin/skill")
+                )
+            ),
+            MenuDTO(
+                name = "Projects",
+                pages = listOf<PageDTO>(
+                    PageDTO(name = "Project", url = "/admin/project"),
+                    PageDTO(name = "ProjectSkill", url = "/admin/project/skill")
+                )
+            )
+        )
+
+        modelAndView?.model?.put("menus", menus)
+    }
+
+}
---
+++ src/main/kotlin/com/yongback/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
@@ -0,0 +1,17 @@
+package com.yongback.portfolio.admin.interceptor
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class AdminInterceptorConfiguration(
+    val adminInterceptor: AdminInterceptor
+) : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(adminInterceptor)
+            .addPathPatterns("/admin/**")
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "/h2**")
+    }
+}
