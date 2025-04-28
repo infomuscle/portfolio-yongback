2b0178f319dabc0b0ae631cbf01dbeb0d72e3c55
infomuscle <infomuscle10@gmail.com>
2025-04-28 23:41:09 +0900

    공통 개발 - DTO

---
+++ src/main/kotlin/com/yongback/portfolio/admin/data/ApiResponse.kt
@@ -0,0 +1,21 @@
+package com.yongback.portfolio.admin.data
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+class ApiResponse<T : Any>(status: HttpStatus) : ResponseEntity<T>(status) {
+
+    companion object {
+        fun successCreate(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 저장되었습니다.")
+        }
+
+        fun successUpdate(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 수정되었습니다.")
+        }
+
+        fun successDelete(): ResponseEntity<Any> {
+            return ResponseEntity.ok("데이터가 삭제되었습니다.")
+        }
+    }
+}
---
+++ src/main/kotlin/com/yongback/portfolio/admin/data/FormElementDTO.kt
@@ -0,0 +1,25 @@
+package com.yongback.portfolio.admin.data
+
+abstract class FormElementDTO(
+    val name: String,
+    val size: Int,
+    val type: String
+)
+
+class TextFormElementDTO(
+    name: String,
+    size: Int
+) : FormElementDTO(name = name, size = size, type = "text")
+
+class DateFormElementDTO(
+    name: String,
+    size: Int
+) : FormElementDTO(name = name, size = size, type = "date")
+
+class SelectFormElementDTO(
+    name: String,
+    size: Int,
+    options: List<Any>
+) : FormElementDTO(name = name, size = size, type = "select") {
+    val options: List<Any> = options
+}
