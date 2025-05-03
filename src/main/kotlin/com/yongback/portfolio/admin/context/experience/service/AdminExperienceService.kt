efe52977f79f88a265312b5a8526e73398b44cf5
infomuscle <infomuscle10@gmail.com>
2025-05-03 15:58:19 +0900

    조회 개발 - 연관관계 있음

---
+++ src/main/kotlin/com/yongback/portfolio/admin/context/experience/service/AdminExperienceService.kt
@@ -0,0 +1,30 @@
+package com.yongback.portfolio.admin.context.experience.service
+
+import com.yongback.portfolio.admin.data.TableDTO
+import com.yongback.portfolio.admin.exception.AdminBadRequestException
+import com.yongback.portfolio.domain.entity.Experience
+import com.yongback.portfolio.domain.entity.ExperienceDetail
+import com.yongback.portfolio.domain.repository.ExperienceRepository
+import org.springframework.stereotype.Service
+
+@Service
+class AdminExperienceService(
+    private val experienceRepository: ExperienceRepository
+) {
+
+    fun getExperienceTable(): TableDTO {
+        val classInfo = Experience::class
+        val entities = experienceRepository.findAll()
+
+        return TableDTO.from(classInfo, entities, "details")
+    }
+
+    fun getExperienceDetailTable(id: Long?): TableDTO {
+        val classInfo = ExperienceDetail::class
+        val entities = if (id != null) experienceRepository.findById(id)
+            .orElseThrow { throw AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수 없습니다.") }
+            .details else emptyList()
+
+        return TableDTO.from(classInfo, entities)
+    }
+}
---
+++ src/main/kotlin/com/yongback/portfolio/admin/context/project/controller/AdminProjectViewController.kt
@@ -0,0 +1,57 @@
+package com.yongback.portfolio.admin.context.project.controller
+
+import com.yongback.portfolio.admin.context.project.service.AdminProjectService
+import com.yongback.portfolio.admin.data.FormElementDTO
+import com.yongback.portfolio.admin.data.SelectFormElementDTO
+import com.yongback.portfolio.admin.data.TextFormElementDTO
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/admin/project")
+class AdminProjectViewController(
+    private val adminProjectService: AdminProjectService
+) {
+
+    @GetMapping
+    fun project(model: Model): String {
+
+        val formElements = listOf<FormElementDTO>(
+            TextFormElementDTO("name", 4),
+            TextFormElementDTO("description", 8),
+            SelectFormElementDTO("startYear", 3, (2010..2030).toList()),
+            SelectFormElementDTO("startMonth", 2, (1..12).toList()),
+            SelectFormElementDTO("endYear", 3, (2010..2030).toList()),
+            SelectFormElementDTO("endMonth", 2, (1..12).toList()),
+            SelectFormElementDTO("isActive", 2, listOf(true.toString(), false.toString()))
+        )
+        model.addAttribute("formElements", formElements)
+
+        val detailFormElements = listOf<FormElementDTO>(
+            TextFormElementDTO("content", 4),
+            TextFormElementDTO("url", 6),
+            SelectFormElementDTO("isActive", 2, listOf(true.toString(), false.toString()))
+        )
+        model.addAttribute("detailFormElements", detailFormElements)
+
+        val table = adminProjectService.getProjectTable()
+        model.addAttribute("table", table)
+
+
+        val detailTable = adminProjectService.getProjectDetailTable(null)
+        model.addAttribute("detailTable", detailTable)
+
+        val pageAttributes = mutableMapOf<String, Any>(
+            Pair("menuName", "Projects"),
+            Pair("pageName", table.name),
+            Pair("editable", true),
+            Pair("deletable", false),
+            Pair("hasDetails", true),
+        )
+        model.addAllAttributes(pageAttributes)
+
+        return "admin/page-table"
+    }
+}
