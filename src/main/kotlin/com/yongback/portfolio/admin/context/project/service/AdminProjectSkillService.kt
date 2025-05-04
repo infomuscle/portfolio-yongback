487ff382c73d4aab4761b5d8a1636454a30f18d7
infomuscle <infomuscle10@gmail.com>
2025-05-04 15:58:46 +0900

    삽입, 삭제 API 개발 - 연관관계 다대다

--- src/main/kotlin/com/yongback/portfolio/admin/context/project/service/AdminProjectSkillService.kt
+++ src/main/kotlin/com/yongback/portfolio/admin/context/project/service/AdminProjectSkillService.kt
@@ -1,7 +1,12 @@
 package com.yongback.portfolio.admin.context.project.service
 
+import com.yongback.portfolio.admin.context.project.form.ProjectSkillForm
 import com.yongback.portfolio.admin.data.TableDTO
+import com.yongback.portfolio.admin.exception.AdminBadRequestException
+import com.yongback.portfolio.admin.exception.AdminInternalServerErrorException
+import com.yongback.portfolio.domain.entity.ProjectSkill
 import com.yongback.portfolio.domain.repository.ProjectRepository
+import com.yongback.portfolio.domain.repository.ProjectSkillRepository
 import com.yongback.portfolio.domain.repository.SkillRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -9,7 +14,8 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class AdminProjectSkillService(
     private val projectRepository: ProjectRepository,
-    private val skillRepository: SkillRepository
+    private val skillRepository: SkillRepository,
+    private val projectSkillRepository: ProjectSkillRepository
 ) {
 
     @Transactional
@@ -50,5 +56,52 @@ class AdminProjectSkillService(
         val skills = skillRepository.findAll()
 
         return skills.map { "${it.id} (${it.name})" }.toList()
+    }
+
+
+    @Transactional
+    fun save(form: ProjectSkillForm) {
+
+        // 이미 매핑된 Project - Skill 여부 검증
+        val projectId = parseId(form.project)
+        val skillId = parseId(form.skill)
+        projectSkillRepository.findByProjectIdAndSkillId(projectId, skillId)
+            .ifPresent { throw AdminBadRequestException("이미 매핑된 데이터입니다.") }
+
+        // 유효한 ProjectSkill 생성
+        val project = projectRepository.findById(projectId)
+            .orElseThrow {
+                throw AdminBadRequestException(
+                    "ID ${projectId}에 해당하는 데이터를 찾을 수 없습니다."
+                )
+            }
+        val skill = skillRepository.findById(skillId)
+            .orElseThrow {
+                throw AdminBadRequestException(
+                    "ID ${skillId}에 해당하는 데이터를 찾을 수 없습니다."
+                )
+            }
+        val projectSkill = ProjectSkill(
+            project = project,
+            skill = skill
+        )
+
+        project.skills.add(projectSkill)
+    }
+
+    private fun parseId(line: String): Long {
+        try {
+            val endIndex = line.indexOf(" ") - 1
+            val id = line.slice(0..endIndex).toLong()
+
+            return id
+        } catch (e: Exception) {
+            throw AdminInternalServerErrorException("ID 추출 중 오류가 발생했습니다.")
+        }
+    }
+
+    @Transactional
+    fun delete(id: Long) {
+        projectSkillRepository.deleteById(id)
     }
 }
