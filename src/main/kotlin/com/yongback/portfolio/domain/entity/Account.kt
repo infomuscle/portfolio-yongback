1124b6694f10118e629d05de5bef655d90953104
infomuscle <infomuscle10@gmail.com>
2025-06-08 17:27:00 +0900

    스프링 시큐리티 - 계정 개발

---
+++ src/main/kotlin/com/yongback/portfolio/domain/entity/Account.kt
@@ -0,0 +1,33 @@
+package com.yongback.portfolio.domain.entity
+
+import jakarta.persistence.*
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+@Entity
+class Account(loginId: String, pw: String) : BaseEntity(), UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    var id: Long? = null
+
+    var loginId: String = loginId
+
+    @Column(name = "password")
+    var pw: String = pw
+
+    override fun getAuthorities() = mutableListOf(SimpleGrantedAuthority("ADMIN"))
+
+    override fun getPassword(): String = pw
+
+    override fun getUsername(): String = loginId
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
